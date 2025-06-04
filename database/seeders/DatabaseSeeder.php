b4dfc16d4946967252b31076c50932fbc548295f
sebastiangavinn <sebastiangavin80@gmail.com>
2025-06-04 13:42:19 +0700

    feat: seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,15 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => 'password'
+        ]);
+
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'superadmin@gmail.com',
+            'password' => 'admin-213QWERTY'
         ]);
     }
 }
